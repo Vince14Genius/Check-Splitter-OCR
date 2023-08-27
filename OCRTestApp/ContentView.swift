8a6ff86f0314469ae66c91f56e1768a367bdb348
Vince14Genius <vince14genius@icloud.com>
2023-08-27 15:07:02 -0700

    Initial Commit

---
+++ OCRTestApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OCRTestApp
+//
+//  Created by Vincent C. on 8/27/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OCRTestApp/OCRTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  OCRTestAppApp.swift
+//  OCRTestApp
+//
+//  Created by Vincent C. on 8/27/23.
+//
+
+import SwiftUI
+
+@main
+struct OCRTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
