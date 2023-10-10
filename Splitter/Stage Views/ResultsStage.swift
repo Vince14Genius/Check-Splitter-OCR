85462f3d78d850eba02f7b6688573e838bc23995
Vince14Genius <vince14genius@icloud.com>
2023-10-09 17:10:05 -0700

    added row animations to the results screen

--- Splitter/Stage Views/ResultsStage.swift
+++ Splitter/Stage Views/ResultsStage.swift
@@ -13,6 +13,8 @@ struct ResultsStage: View {
     @Binding var flowState: SplitterFlowState
     let currency: Currency
     
+    private let animationDelayPerRow = 0.2
+    
     private var result: CalculationResult? {
         guard let totalCost = flowState.totalCost else {
             return nil
@@ -29,40 +31,25 @@ struct ResultsStage: View {
         NavigationStack {
             ScrollView(.vertical) {
                 VStack {
-                    ForEach(result?.payers ?? [], id: \.self) { payer in
-                        VStack {
-                            HStack {
-                                Text(payer.name)
-                                Spacer()
-                                Text(payer.payerTotal.formatted(.currency(code: currency.rawValue)))
-                                    .bold()
-                                    .textSelection(.enabled)
-                            }
-                            .font(.title3)
-                            
-                            ForEach(payer.items, id: \.self) { item in
-                                HStack {
-                                    Text(item.name)
-                                    Spacer()
-                                    Text("× \(item.realQuantity.roundedToTwoPlaces)")
-                                }
-                            }
-                            .foregroundStyle(.secondary)
+                    if let result {
+                        ForEach(result.payers.indices, id: \.self) { i in
+                            ResultsRow(
+                                payer: result.payers[i],
+                                currency: currency,
+                                animationDelay: Double(i) * animationDelayPerRow
+                            )
                         }
-                        .padding()
-                        .background(Color(.secondarySystemBackground))
-                        .cornerRadius(12)
+                        Divider()
+                        HStack {
+                            Text("Total: ")
+                            Spacer()
+                            Text(result.totalCost.formatted(.currency(code: currency.rawValue)))
+                                .bold()
+                                .textSelection(.enabled)
+                        }
+                        .font(.title2)
+                        .padding(.horizontal)
                     }
-                    Divider()
-                    HStack {
-                        Text("Total: ")
-                        Spacer()
-                        Text(result?.totalCost.formatted(.currency(code: currency.rawValue)) ?? "")
-                            .bold()
-                            .textSelection(.enabled)
-                    }
-                    .font(.title2)
-                    .padding(.horizontal)
                 }
                 .padding()
             }
@@ -73,6 +60,49 @@ struct ResultsStage: View {
             ToolbarItem(placement: .bottomBar) {
                 ResultsStageNavBar(stage: $stage, path: $path, flowState: $flowState)
             }
+        }
+    }
+}
+
+private struct ResultsRow: View {
+    let payer: ResultPayer
+    let currency: Currency
+    let animationDelay: TimeInterval
+    
+    @State private var isVisible = false
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Text(payer.name)
+                Spacer()
+                Text(payer.payerTotal.formatted(.currency(code: currency.rawValue)))
+                    .bold()
+                    .textSelection(.enabled)
+            }
+            .font(.title3)
+            
+            ForEach(payer.items, id: \.self) { item in
+                HStack {
+                    Text(item.name)
+                    Spacer()
+                    Text("× \(item.realQuantity.roundedToTwoPlaces)")
+                }
+            }
+            .foregroundStyle(.secondary)
+        }
+        .padding()
+        .background(Color(.secondarySystemBackground))
+        .cornerRadius(12)
+        .opacity(isVisible ? 1.0 : 0.0)
+        .offset(x: isVisible ? 0.0 : -300.0)
+        .onAppear {
+            withAnimation(.bouncy.delay(animationDelay)) {
+                isVisible = true
+            }
+        }
+        .onDisappear {
+            isVisible = false
         }
     }
 }
